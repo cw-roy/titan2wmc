4e835ad10a369f18ea6df02db0952a73d489f011
cw-roy <curtis_roy@msn.com>
2025-03-31 16:19:05 -0700

    Revert "Split utilities into separate module"
    This reverts commit de99a51adcf028ca3b4e922ba431bd18d5207ee9.

--- utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def get_current_start_time():
-    """Generate start time based on the current date and time."""
-    now = datetime.now()  # Get current date and time
-    start_time = now.strftime("%Y%m%d%H%M")  # Format the date as 'YYYYMMDDHHMM'
-    return start_time
