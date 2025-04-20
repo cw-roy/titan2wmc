4c57f4a19e14dafed6d3251258b872d43ba54edb
cw-roy <curtis_roy@msn.com>
2025-04-20 08:36:52 -0700

    Fixed problem with auth.

--- titantv/auth.py
+++ titantv/auth.py
@@ -8,13 +8,21 @@ from dotenv import load_dotenv
 
 def load_credentials():
     load_dotenv()
+
+    # Define the environment variables and their corresponding names
     credentials = {
         "user_id": os.getenv("TITANTV_USER_ID"),
+        "lineup_id": os.getenv("TITANTV_LINEUP_ID"),
         "login_name": os.getenv("TITANTV_USERNAME"),
         "password": os.getenv("TITANTV_PASSWORD"),
     }
-    if not all(credentials.values()):
+
+    # Check for missing variables and build a list of missing ones
+    missing_vars = [key for key, value in credentials.items() if value is None]
+
+    if missing_vars:
         raise ValueError(
-            "Missing required environment variables: TITANTV_USER_ID, TITANTV_USERNAME, TITANTV_PASSWORD"  # noqa: E501
-        )  # noqa: E501
+            f"Missing required environment variables: {', '.join(missing_vars)}"
+        )
+
     return credentials
